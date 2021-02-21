901d9af7fc47f466630c707106b5a6e5ea35dfbf
a-johanes <antoniusjohanes99@gmail.com>
2021-02-21 15:29:47 +0700

    Implements register

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const createError = require('http-errors');
 const router = express.Router();
+const Auth = require('../Models/Auth.model');
+
+function checkErrors(error) {
+  if (error) {
+    if (error.errors['username']) {
+      throw createError.BadRequest(error.errors['username'].message);
+    }
+    if (error.errors['password']) {
+      throw createError.BadRequest(error.errors['password'].message);
+    }
+  }
+}
 
 router.post('/register', async (req, res, next) => {
-  res.send('register route');
+  try {
+    const newAuth = await new Auth(req.body).save();
+
+    res.send(newAuth);
+  } catch (error) {
+    console.log(error.message);
+
+    let errorKeyString = '';
+    if (error.errors['username']) {
+      errorKeyString = 'username';
+    } else if (error.errors['password']) {
+      errorKeyString = 'password';
+    }
+
+    if (errorKeyString) return next(createError.UnprocessableEntity(error.errors[errorKeyString].message));
+
+    next(error);
+  }
 });
 
 router.post('/login', async (req, res, next) => {
