6850f8f8f6f26d15e954f51e596dc1d0110cab3a
a-johanes <antoniusjohanes99@gmail.com>
2021-02-21 14:34:37 +0700

    Init auth route

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const app = express();
+
 require('dotenv').config();
 require('./Utils/init_mongodb');
-const app = express();
+
+const AuthRoute = require('./Routes/Auth.route');
 
 app.get('/', (req, res, next) => {
   res.send('HOME');
 });
+
+app.use('/auth', AuthRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
