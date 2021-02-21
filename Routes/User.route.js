acc19fb987f5084afb28593061038b4a1e017bff
a-johanes <antoniusjohanes99@gmail.com>
2021-02-21 16:34:48 +0700

    Implements basic login

--- Routes/User.route.js
+++ Routes/User.route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const createError = require('http-errors');
 const router = express.Router();
 const User = require('../Models/User.model');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
 getErrors = (error) => {
   if (error.errors['username']) {
@@ -20,22 +22,39 @@ router.post('/register', async (req, res, next) => {
   } catch (error) {
     console.log(error.message);
 
-    if (error.code == 11000) {
-      // dulicate key error
-      const username = error.keyValue[Object.keys(error.keyValue)[0]];
-      return next(createError.Conflict(`${username} is already been registered`));
+    if (error instanceof mongodb.MongoError) {
+      if (error.code == 11000) {
+        // dulicate key error
+        const username = error.keyValue[Object.keys(error.keyValue)[0]];
+        return next(createError.Conflict(`${username} is already been registered`));
+      }
     }
 
-    let errorKeyString = getErrors(error);
-
-    if (errorKeyString) return next(createError.UnprocessableEntity(error.errors[errorKeyString].message));
-
+    if (error instanceof mongoose.Error.ValidationError) {
+      let errorKeyString = getErrors(error);
+      if (errorKeyString) return next(createError.UnprocessableEntity(error.errors[errorKeyString].message));
+    }
     next(error);
   }
 });
 
 router.post('/login', async (req, res, next) => {
-  res.send('login route');
+  try {
+    await User.validate(req.body);
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) throw createError.NotFound('User not registered');
+
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+
+    if (error instanceof mongoose.Error.ValidationError) {
+      let errorKeyString = getErrors(error);
+      if (errorKeyString) return next(createError.UnprocessableEntity(error.errors[errorKeyString].message));
+    }
+
+    next(error);
+  }
 });
 
 router.post('/refresh-token', async (req, res, next) => {
